4b3fc2995b505d2e428cc430d3c5c578f8b352f9
Scott Truong <asdfgscott@gmail.com>
2021-09-10 00:39:56 -0400

    Made sure we can get user input

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Bela
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line = Console.ReadLine();
+            Console.WriteLine(line);
         }
     }
 }
